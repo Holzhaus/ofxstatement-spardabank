a11afe23663fcc87641a487dc157738f9977d102
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2024-12-10 00:27:42 +0100

    chore: Update default template

--- src/ofxstatement_de_spardabank/plugin.py
+++ src/ofxstatement_de_spardabank/plugin.py
@@ -5,14 +5,14 @@ from ofxstatement.parser import StatementParser
 from ofxstatement.statement import Statement, StatementLine
 
 
-class SamplePlugin(Plugin):
-    """Sample plugin (for developers only)"""
+class SpardaBankDePlugin(Plugin):
+    """Plugin for parsing CSV files from the German Sparda-Bank eG."""
 
-    def get_parser(self, filename: str) -> "SampleParser":
-        return SampleParser(filename)
+    def get_parser(self, filename: str) -> "SpardaBankDeParser":
+        return SpardaBankDeParser(filename)
 
 
-class SampleParser(StatementParser[str]):
+class SpardaBankDeParser(StatementParser[str]):
     def __init__(self, filename: str) -> None:
         super().__init__()
         self.filename = filename
